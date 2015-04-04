2b0c3a473d48b2bb89248e17e51807cdb96277f0
Moci1 <bukta.mate@gmail.com>
2015-04-04 20:45:58 +0200

    First commit

--- ControlKit/Spritebatch.cs
+++ ControlKit/Spritebatch.cs
@@ -85,6 +85,32 @@ namespace glControlKit
 
 			return id;
 		}
+		public static int LoadTexture(string filename, TextureWrapMode wrapMode)
+		{
+			if (String.IsNullOrEmpty(filename))
+				throw new ArgumentException(filename);
+
+			int id = GL.GenTexture();
+			GL.BindTexture(TextureTarget.Texture2D, id);
+
+			Bitmap bmp = new Bitmap(filename);
+			BitmapData bmp_data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+			GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bmp_data.Width, bmp_data.Height, 0,
+			              OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, bmp_data.Scan0);
+
+			bmp.UnlockBits(bmp_data);
+
+			// We haven't uploaded mipmaps, so disable mipmapping (otherwise the texture will not appear).
+			// On newer video cards, we can use GL.GenerateMipmaps() or GL.Ext.GenerateMipmaps() to create
+			// mipmaps automatically. In that case, use TextureMinFilter.LinearMipmapLinear to enable them.
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)wrapMode);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)wrapMode);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+			return id;
+		}
 		public void DrawTexture(Texture2D texture, Vector2 pos, Vector2 scale, Vector2 origin) {
 			GL.Enable(EnableCap.Blend);
 			int[] argb = null;
